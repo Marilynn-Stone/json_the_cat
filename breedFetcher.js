e0d9e5d1d79e095ebf812da0e87ee5a9aede9344
Marilynn Stone <hello@marilynnstone.com>
2022-06-21 13:53:06 +0000

    work on edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,18 @@
 const request = require('request');
 const argString = process.argv.slice(2);
+let data;
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${argString[0]}`, (error, response, body) => {
   console.log('error:', error);
   console.log('statusCode:', response && response.statusCode);
-  const data = JSON.parse(body);
-  console.log(data[0]['description']);
+  data = JSON.parse(body);
+  output(data);
 });
+
+const output = function() {
+  if (data === void 0) {
+    console.log("Breed not found");
+  } else {
+    console.log(data[0]['description']);
+  }
+};
