0d009968b0c49ef3534903af4169a94f7c1b8597
Marilynn Stone <hello@marilynnstone.com>
2022-06-22 23:56:13 +0000

    create error message and refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,10 +3,9 @@ const argString = process.argv.slice(2);
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${argString[0]}`, (error, response, body) => {
   if (error) {
-    console.log('error:', error);
+    console.log("Request Failed.\n", error);
     return;
   }
-  console.log('statusCode:', response && response.statusCode);
   const data = JSON.parse(body);
   if (data.length === 0) {
     console.log("Breed not found");
