f9e66151101228532d13a22b43ba301105a3ab00
Marilynn Stone <hello@marilynnstone.com>
2022-06-22 23:51:17 +0000

    handle breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,16 @@
 const request = require('request');
 const argString = process.argv.slice(2);
-let data;
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${argString[0]}`, (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  data = JSON.parse(body);
-  output(data);
-});
-
-const output = function() {
-  if (data === void 0) {
-    console.log("Breed not found");
-  } else {
-    console.log(data[0]['description']);
+  if (error) {
+    console.log('error:', error);
+    return;
   }
-};
+  console.log('statusCode:', response && response.statusCode);
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log("Breed not found");
+    return;
+  }
+  console.log(data[0].description);
+});
